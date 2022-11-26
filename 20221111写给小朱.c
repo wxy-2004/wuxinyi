8e335ad3ee44ac1836adfee7c322b4f05572f011
wxy-2004 <118707813+wxy-2004@users.noreply.github.com>
2022-11-26 12:02:32 +0800

    分支语句：if，switch 和 循环语句：for 应用
    顺序结构，分支结构，循环结构
    分支语句：if，switch
    循环语句：while，do while，for

---
+++ 20221111写给小朱.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+int main()
+{
+    int day1=0;
+    int day2=0;
+    int day3=0;
+    int i =0;
+    int day4=0;
+    printf ("\n  今天是在一起的第几天?\n");
+    scanf ("%d",&day1 );
+    if (day1==618)
+    {
+        printf ("  输入正确啦,我爱你!\n\n");
+    }
+        else
+        {
+            printf ("  回答错啦,怎么个事?\n");
+            printf ("  记住了今天是在一起的第618天!\n\n");
+        }
+    printf ("  接下来再输入任意一个在一起过的天数吧\n\n");
+    scanf ("%d",&day2);
+    
+    if (day2>0&&day2<30)
+    {
+        printf ("  在一起快一个月啦！\n  欢迎来到我的世界。\n");
+    }
+    else if (day2 ==30)
+    {
+        printf ("  第30天，非常爱朱明晖！\n");
+    }
+    else if (day2>30&&day2<60)
+    {
+        printf ("  在一起快两个月啦！\n  一些小美好正在井然有序的发生着。\n");
+    }
+    else if (day2 ==60)
+    {
+        printf ("  第60天，非常爱朱明晖！\n");
+    }
+    else if (day2 >60&&day2 <90)
+    {
+        printf ("  在一起快三个月啦！\n  是让你远远听见的风。\n");
+    }
+    else if (day2 ==90)
+    {
+        printf ("  第90天，非常爱朱明晖！\n");
+    }
+    else if (day2>90&&day2 <120)
+    {
+        printf ("  在一起快四个月啦！\n  我的世界下雨会有你来撑伞。\n");
+    }
+    else if (day2 ==120)
+    {
+        printf ("  第120天，非常爱朱明晖！\n");
+    }
+    else if (day2>120&& day2<150)
+    {
+        printf ("  在一起快五个月啦！\n  没有你的旅行交流浪。\n");
+    }
+    else if (day2 ==150)
+    {
+        printf ("  第150天，非常爱朱明晖！\n");
+    }
+    else if (day2 >150&&day2<180)
+    {
+        printf ("  在一起快六个月啦！\n  你要好好的，花会好好的。\n");
+    }
+    else if (day2 ==180)
+    {
+        printf ("  第180天，非常爱朱明晖！\n");
+    }
+    else if (day2>180&&day2<210)
+    {
+        printf ("  在一起快七个月啦！\n  总会有人把小笨蛋的玫瑰养在垃圾桶里。\n");
+    }
+    else if (day2 ==210)
+    {
+        printf ("  第210天，非常爱朱明晖！\n");
+    }
+    else if (day2>210&&day2<240)
+    {
+        printf ("  在一起快八个月啦！\n  听，花落的声音，我对你的爱意。\n");
+    }
+    else if (day2 ==240)
+    {
+        printf ("  第240天，非常爱朱明晖！\n");
+    }
+    else if (day2>240&&day2<270)
+    {
+        printf ("  在一起快九个月啦!\n  永远真诚，永远简单。\n");
+    }
+    else if (day2 ==270)
+    {
+        printf ("  第270天，非常爱朱明晖！\n");
+    }
+    else if (day2>270&&day2<300)
+    {
+        printf ("  在一起快十个月啦！\n  生活再平凡，也是限量版。\n");
+    }
+    else if (day2 ==300)
+    {
+        printf ("  第300天，非常爱朱明晖！\n");
+    }
+    else if (day2>300&&day2 <330)
+    {
+        printf ("  在一起快十一个月啦！\n  他们说答案都在路上。\n");
+    }
+    else if (day2 ==330)
+    {
+        printf ("  第330天，非常爱朱明晖！\n");
+    }
+    else if (day2 >330&&day2 <360)
+    {
+        printf ("  在一起快十二个月啦！\n  玫瑰在山顶与夕阳相遇。\n");
+    }
+    else if (day2 ==360)
+    {
+        printf ("  第360天，非常爱朱明晖！\n");
+    }
+    else if (day2>360&&day2 <390)
+    {
+        printf ("  在一起快十三个月啦！\n  森林的尽头是阳光。\n");
+    }
+    else if (day2 ==390)
+    {
+        printf ("  第390天，非常爱朱明晖！\n");
+    }
+    else if (day2 >390&&day2 <420)
+    {
+        printf ("  在一起快十四个月啦！\n  杂七杂八的日常。\n");
+    }
+    else if (day2 ==420)
+    {
+        printf ("  第420天，非常爱朱明晖！\n");
+    }
+    else if (day2 >420&&day2 <450)
+    {
+        printf ("  在一起快十五个月啦！\n  好事总会发生在下一个转弯。\n");
+    }
+    else if (day2 ==450)
+    {
+        printf ("  第450天，非常爱朱明晖！\n");
+    }
+    else if (day2 >450&&day2<480)
+    {
+        printf ("  在一起快十六个月啦！\n  现在几点，开心一点。\n");
+    }
+    else if (day2 ==480)
+    {
+        printf ("  第480天，非常爱朱明晖！\n");
+    }
+    else if (day2 >480&&day2 <510)
+    {
+        printf ("  在一起快十七个月啦！\n  明天依旧漂亮。\n");
+    }
+    else if (day2 ==510)
+    {
+        printf ("  第510天，非常爱朱明晖！\n");
+    }
+    else if (day2 >510&&day2 <540)
+    {
+        printf ("  在一起快十八个月啦！\n  季节轮换，我们不变。\n");
+    }
+    else if (day2 ==540)
+    {
+        printf ("  第540天，非常爱朱明晖！\n");
+    }
+    else if (day2>540&&day2 <570)
+    {
+        printf ("  在一起快十九个月啦！\n  春早秋晚，岁岁平安。\n");
+    }
+    else if (day2 ==570)
+    {
+        printf ("  第570天，非常爱朱明晖！\n");
+    }
+    else if (day2 >570&&day2 <600)
+    {
+        printf ("  在一起快二十个月啦！\n  不见面的浪漫是倒数快要见面的日子。\n");
+    }
+    else if (day2 ==600)
+    {
+        printf ("  第600天，非常爱朱明晖！\n");
+    }
+    else if (day2 >600&&day2 <=618)
+    {
+        printf ("  在一起快二十一个月啦！\n  思念能再兑换一面吗，我亲爱的朱明晖。\n");
+    }
+    else
+    {
+        printf ("  还没有和你一起度过这么多天呢，期待接下来的日子。\n");
+        printf ("  爱武馨怡这样的美女是不会有错的！\n\n");
+    }
+    printf ("  \n扣“1”对你说爱你！\n\n");
+    scanf ("%d",&day3 );
+    switch (day3 )
+    {
+        case 1:printf ("  嘻嘻，我爱你！\n");
+            break ;
+        default :printf ("  真不听话！\n  算啦，勉勉强强说一句爱你吧，嘻嘻。\n\n");
+            break ;
+    }
+    printf ("  再扣“1”，嘻嘻。\n\n");
+    scanf ("%d",&day4);
+    switch (day4)
+    {
+        case 1: printf ("  朱明晖你永远是武馨怡最爱的人！\n");
+            for (i =1;i <=2;i ++)
+            {
+                printf ("  永远是，永远是！\n");
+            }
+            break ;
+        default :printf ("  真是不听话！！！\n  不听话也永远是武馨怡最爱的人哦！\n");
+            for (i =1;i <=2;i ++)
+            {
+                printf ("  永远是，永远是！\n");
+            }
+            break ;
+    }
+    printf ("\n  你也要永远永远爱武馨怡哦！\n\n");
+    return 0;
+}
